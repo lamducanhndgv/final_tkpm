5f7b0ce0ffd8b0ac6ec3d4e4fe2101c84fadc914
lamducanhndgv <lamducanhndgv@gmail.com>
2020-09-03 20:24:04 +0700

    [DONE] done server for inference

---
+++ code/main.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from Command import run
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+app = Flask(__name__)
+app.register_blueprint(run.run_model_page)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello World</h1>"
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0")
