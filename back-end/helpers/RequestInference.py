accc8c0073e9e52da9b96e1240f45e6c9fc0eb52
lamducanhndgv <lamducanhndgv@gmail.com>
2020-09-04 20:24:28 +0700

    [NEW] add more test in RequestInference

--- back-end/helpers/RequestInference.py
+++ back-end/helpers/RequestInference.py
@@ -11,10 +11,11 @@ class Framework(Enum):
 
     @classmethod
     def generate_framework_id(cls, framework_name):
-        return eval(f"cls.{framework_name}")
+        return eval(f"cls.{framework_name.upper()}")
 
 
 class RequestInference:
+    TESTING = 0
     _hosts = ["url1", "url2"]
 
     def __init__(self, userid, modelid, img):
@@ -24,7 +25,8 @@ class RequestInference:
         self._framework = None
 
     def read_config_file(self):
-        config_file_path = os.path.join("/data/Users/", self._userid, self._modelid, "config.json")
+        config_file_path = "config.json" if self.TESTING \
+            else os.path.join("/data/Users/", self._userid, self._modelid, "config.json")
         with open(config_file_path, "r") as file:
             data = json.load(file)
             framework = data[0]["framework"]
@@ -34,8 +36,15 @@ class RequestInference:
         if self._framework is None:
             self.read_config_file()
         host_id = Framework.generate_framework_id(self._framework)
-        send_url = self._hosts[host_id]
-        response = requests.get(send_url, json={"img": self._img})
-        if response.status_code == 200:
-            return response.json()
-        return False
+        send_url = self._hosts[host_id.value]
+        if not self.TESTING:
+            response = requests.get(send_url, json={"img": self._img})
+            if response.status_code == 200:
+                return response.json()
+        return False if not self.TESTING else send_url
+
+
+# Testing
+if __name__ == "__main__":
+    RequestInference.TESTING = 1
+    print(RequestInference("user01", "model01", "superimage")())
